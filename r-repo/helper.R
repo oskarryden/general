69804ea338e1f167551245cd4be1cfbd4f4f8d8e
oskarryden <rydenoskar@gmail.com>
2023-07-03 19:44:41 +0200

    save

--- r-repo/helper.R
+++ r-repo/helper.R
@@ -18,33 +18,12 @@ get_base_r_packages <- function(){
     sort(rownames(installed.packages(priority="base")))
 }
 
+prune_base_r_packages <- function(x) {
+    x[!x %in% get_base_r_packages()]
+}
 
-# Helper for {show_package_description}
-.capture_description_field <- function(pattern, description_file) {
-    sidx <- grep(sprintf("^%s:", pattern), description_file)
-    stopifnot(length(sidx) == 1 & sidx > 0)
-    
-    sidx_end <- sidx + 1
-    repeat {
-        if (grepl("^\\s", description_file[sidx_end])) {
-            sidx_end <- sidx_end + 1
-        } else {
-            sidx_end <- sidx_end - 1
-            break
-        }
-    }
+# View DESCRIPTION file of a {package} in {directory}
+# TODO: Update this function to use {utils::packageDescription}
+show_package_description <- function(package, directory) {
 
-    field_rows <- seq(from = sidx, to = sidx_end, by = 1)
-    field <-
-        gsub(pattern = "\\s{2,}", replacement = "", x = 
-            trimws(
-                strsplit(
-                    strsplit(
-                        paste0(description_file[field_rows], collapse = ""),
-                            ":")[[1]][2],
-                                ",")[[1]]
-                                    )
-                                        )
-    out <- toString(field)
-    return(out)
 }
